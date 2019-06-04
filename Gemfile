ec6259ebf47df598f6f2aded0fd7111bfa42f479
light130 <light13silver@gmail.com>
2019-06-04 18:37:26 +0900

    Change layout and routes

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.6.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
+#Use Bootstrap
+gem 'bootstrap', '~> 4.3.1'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,8 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  #test環境でもApplicationHelperを使えるようにする
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
